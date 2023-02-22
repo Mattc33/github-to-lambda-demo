fad6550500d8c7c1aa21184a80deb0b40a63903a
Matthew Chan <matthew.chan@slalom.com>
2023-02-22 00:13:02 -0500

    update

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,3 @@
-import pandas as pd
-
 def lambda_handler(event, context);
-    data = {'col1': [1,2], 'col2': [3,4]}
-    dataframe = pd.DataFrame(data=data)
-    print(dataframe)
-    print('Done x1')
+  hello = 'Hello World'
+  print(hello)
